db966ab4137189443e944cb818c6eb981275dc79
Ian Whitlock <isw@juniper.net>
2014-10-22 18:41:21 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='IansFace', 
+      version='1.0',
+      description='A module for printing Ian\'s face.',
+      author='Ian',
+      author_email='giggles@alum.hackerschool.com',
+      url='http://giggles.hashbang.sh',
+      py_modules=['iansface'])
